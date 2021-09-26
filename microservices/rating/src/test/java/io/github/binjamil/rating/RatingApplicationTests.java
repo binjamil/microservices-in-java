26c2c07804f30d106825d1e76ce2d72add1230f7
Muhammad Bin Jamil <binjamil35@gmail.com>
2021-09-26 22:59:02 +0500

    Add rating microservice base

---
+++ microservices/rating/src/test/java/io/github/binjamil/rating/RatingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.binjamil.rating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
